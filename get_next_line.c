46cb02dffd2d5b9531bc726cc4d35fcf46d86b8d
Lucas Perrin <99961102+OppositeThanks@users.noreply.github.com>
2022-05-23 11:37:25 +0200

    Updated normal gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,13 @@
 /*   By: lperrin <lperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 11:17:01 by lperrin           #+#    #+#             */
-/*   Updated: 2022/05/13 11:59:19 by lperrin          ###   ########.fr       */
+/*   Updated: 2022/05/23 11:20:04 by lperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* Please note that the returned line should include the terminating \n character,
 except if the end of file was reached and does not end with a \n character. */
+#include "get_next_line.h"
 
 char	*read_next_line(int fd, char *pre_next_str)
 {
@@ -27,7 +28,7 @@ char	*read_next_line(int fd, char *pre_next_str)
 		bytes = read(fd, buffer, BUFFER_SIZE);
 		if (bytes == -1)
 		{
-			free(buff);
+			free(buffer);
 			return (NULL);
 		}
 		buffer[bytes] = '\0';
@@ -40,13 +41,19 @@ char	*read_next_line(int fd, char *pre_next_str)
 char	*get_next_line(int fd)
 {
 	char		*line;
-	static char	*pre_next_str;							//Vaut null au début
+	static char	*pre_next_str;
+
+	/* Vaut null au début */
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	pre_next_str = read_next_line(fd, pre_next_str);	//va prendre la premiere et seconde ligne dans prev_next_str
+	pre_next_str = read_next_line(fd, pre_next_str);
+	/* Va prendre la premiere et seconde ligne dans prev_next_str */
 	if (!pre_next_str)
 		return (NULL);
-	line = ft_get_line(pre_next_str);					//Va trouver la ligne correspondante dans pre_next_str
-	pre_next_str = ft_new_sec_str(pre_next_str);		//Va changer la valeur de static char pre_next_str pour pouvoir relancer le programme
+	line = ft_get_line(pre_next_str);
+	/* Va trouver la ligne correspondante dans pre_next_str */
+	pre_next_str = ft_new_next_str(pre_next_str);
+	/* Va changer la valeur de static char 
+	pre_next_str pour pouvoir relancer le programme */
 	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lperrin <lperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 11:25:53 by lperrin           #+#    #+#             */
-/*   Updated: 2022/05/11 11:27:35 by lperrin          ###   ########.fr       */
+/*   Updated: 2022/05/23 11:09:39 by lperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
+char	*read_next_line(int fd, char *pre_next_str);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *str, int n);
+char	*ft_strjoin(char *first, char *second);
+char	*ft_get_line(char *pre_next_str);
+char	*ft_new_next_str(char *pre_next_str);
 
 #endif
